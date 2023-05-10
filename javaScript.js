459f7c3968996f46cb322c8f75b489f2e5524f8e
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-05-10 00:46:41 -0400

    api implementada

--- javaScript.js
+++ javaScript.js
@@ -5,10 +5,12 @@ const BASE_URL ='http://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
 + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const searchURL = BASE_URL + '/search/movie?'+ API_KEY;
 
-const main = document.getElementById('main')
-
+// variables globales
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search')
 
 getMovies(API_URL);
 
@@ -53,3 +55,28 @@ function getColor(vote) {
         return 'red'
     }
 }
+
+// intervalo de slider
+
+document.addEventListener("DOMContentLoaded", function() {
+    new bootstrap.Carousel(document.getElementById("carouselExampleControls"), {
+        interval: 4000, // Ajusta el valor a la cantidad de milisegundos que deseas para la duración de cada diapositiva
+        pause: false
+    });
+});
+
+
+
+// formulario de buscar
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    
+    if(searchTerm){
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+})
