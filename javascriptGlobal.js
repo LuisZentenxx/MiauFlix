9b9de5b9a900fd2f8add04b3cc3fcf1907f4c6d4
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-05-11 01:26:50 -0400

    subiendo

--- javascriptGlobal.js
+++ javascriptGlobal.js
@@ -1,3 +1,6 @@
+
+// intervalo de slider
+
 document.addEventListener("DOMContentLoaded", function() {
     new bootstrap.Carousel(document.getElementById("carouselExampleControls"), {
         interval: 4000, // Ajusta el valor a la cantidad de milisegundos que deseas para la duración de cada diapositiva
